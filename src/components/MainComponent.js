051943d8550efb86c4f5630d7d16624ee6079e92
matteoraccone <matteo.raccone@gmail.com>
2020-10-19 00:00:03 +0200

    React Router

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,9 +1,14 @@
 import React, { Component } from 'react';
-import Menu from './MenuComponent';
-import DishDetail from './DishdetailComponent';
+import { Navbar, NavbarBrand } from 'reactstrap';
+import Home from './HomeComponent';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
+import Menu from './MenuComponent';
+import DishDetail from './DishdetailComponent';
 import { DISHES } from '../shared/dishes';
+
+import { Switch, Route, Redirect } from 'react-router-dom';
+
 
 class Main extends Component {
 
@@ -20,12 +25,23 @@ class Main extends Component {
   }
 
   render() {
+
+    const HomePage = () => {
+      return(
+          <Home
+          />
+      );
+    }
+
     return (
       <div>
-        <Header />
-        <Menu dishes={this.state.dishes} onClick={(dishId) => this.onDishSelect(dishId)} />
-        <DishDetail dish={this.state.dishes.filter((dish) => dish.id === this.state.selectedDish)[0]} />
-        <Footer />
+        <Header/>
+        <Switch>
+          <Route path='/home' component={HomePage} />
+          <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} />} />
+          <Redirect to="/home" />
+        </Switch>
+        <Footer/>
       </div>
     );
   }
--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -1,30 +1,27 @@
-import React from 'react';
-import { Card, CardImg, CardImgOverlay,
-    CardTitle } from 'reactstrap';
+import React from "react";
+import { Card, CardImg, CardTitle, CardImgOverlay} from "reactstrap";
 
-    function RenderMenuItem ({dish, onClick}) {
-        return (
-            <Card
-                onClick={() => onClick(dish.id)}>
+    function RednerMenuItem({dish, onClick}) {
+        return(
+            <Card>
                 <CardImg width="100%" src={dish.image} alt={dish.name} />
-                <CardImgOverlay>
-                    <CardTitle>{dish.name}</CardTitle>
-                </CardImgOverlay>
+                    <CardImgOverlay>
+                        <CardTitle>{dish.name}</CardTitle>
+                    </CardImgOverlay>
             </Card>
         );
     }
 
     const Menu = (props) => {
 
-        const menu = props.dishes.map((dish) => {
-            return (
-                <div className="col-12 col-md-5 m-1"  key={dish.id}>
-                    <RenderMenuItem dish={dish} onClick={props.onClick} />
-                </div>
-            );
+        const menu = props.dishes.map((dish) =>{
+        return(
+            <div key={dish.id} className="col-12 col-md-5 m-1">
+                <RednerMenuItem dish={dish} onClick={props.onClick} />
+            </div>
+            )
         });
-
-        return (
+        return(
             <div className="container">
                 <div className="row">
                     {menu}
