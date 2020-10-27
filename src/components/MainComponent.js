d7c82ca8b27c7ee21bf16c734ab5512b1c8dd634
matteoraccone <matteo.raccone@gmail.com>
2020-10-27 22:17:15 +0100

    controlled forms

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -3,11 +3,13 @@ import Home from './HomeComponent';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import Menu from './MenuComponent';
+import Contact from './ContactComponent';
+import About from './AboutComponent';
+import DishDetail from './DishdetailComponent';
 import { DISHES } from '../shared/dishes';
 import { COMMENTS } from '../shared/comments';
 import { PROMOTIONS } from '../shared/promotions';
 import { LEADERS } from '../shared/leaders';
-import Contact from './ContactComponent';
 import { Switch, Route, Redirect } from 'react-router-dom';
 
 
@@ -35,13 +37,22 @@ class Main extends Component {
         );
       }
 
+    const DishWithId = ({match}) => {
+    return(
+        <DishDetail dish={this.state.dishes.filter((dish) => dish.id === parseInt(match.params.dishId,10))[0]} 
+            comments={this.state.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId,10))} />
+    );
+    };
+
     return (
       <div>
         <Header/>
         <Switch>
           <Route path='/home' component={HomePage} />
           <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} />} />
+          <Route path='/menu/:dishId' component={DishWithId} />
           <Route exact path='/contactus' component={Contact} />} />
+          <Route path='/aboutus' component={() => <About leader= {this.state.leaders}></About>} />
           <Redirect to="/home" />
         </Switch>
         <Footer/>
